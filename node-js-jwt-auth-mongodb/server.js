03efbfe42a8afab067ef9d3a2518dedce29cc5e8
RamanSB <ramansb100@gmail.com>
2021-04-11 17:30:47 +0100

    Upon successful connection to MongoDB add 3 rows in the roles collection

--- node-js-jwt-auth-mongodb/server.js
+++ node-js-jwt-auth-mongodb/server.js
@@ -27,3 +27,54 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+//Connecting to MongoDB
+const db = require("./app/models");
+const Role = db.role;
+
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig:PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connected to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Error connecting to MongoDB...", err);
+    process.exit();
+  });
+
+function initial(){
+  Role.estimatedDocumentCount((err, count) => {
+    if(!err && count === 0){
+      new Role({
+        name: "user"
+      }).save(err => {
+        if(err){
+          console.log("Error", err);
+        }
+        console.log("Added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if(err){
+          console.log("Error", err);
+        }
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if(err){
+          console.log("Error", err);
+        }
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
